534a76d289043026cf12225733c0f022303f0427
Poovarasu <poovarasu@mallow-tech.com>
2020-05-23 15:16:49 +0530

    added me

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,11 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
-Route::group(['prefix' => 'auth'], function () {
+Route::group(['prefix' => 'v1/auth'], function () {
     Route::post('login', 'Api\V1\AuthController@login');
     Route::post('refresh', 'Api\V1\AuthController@refresh');
     Route::group(['middleware' => 'jwt.auth'], function () {
+        Route::post('me', 'Api\v1\AuthController@me');
         Route::post('logout', 'Api\V1\AuthController@logout');
     });
 });
